421979b1e27c8bd291331c7215eb31f927cebedc
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-07-11 15:14:48 -0300

    Aula08

---
+++ Modulo12 (Javascript - Básico [Lógica de programação])/Aula08 (Operadores Comparativos)/script.js
@@ -0,0 +1,16 @@
+// == igual a 
+// === valor igual e igual
+// != não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior que ou igual a
+// <= menor ou igual a
+
+var a = 5;
+var b = 4;
+
+console.log(a <= b);
+
+
+
