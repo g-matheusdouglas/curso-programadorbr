35f77a1c49cf42eb44355aaa1b4624025d55c038
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-07-11 15:02:40 -0300

    Aula07

---
+++ Modulo12 (Javascript - Básico [Lógica de programação])/Aula07 (Atribuições)/script.js
@@ -0,0 +1,13 @@
+// =    x = y   x = y
+// +=   x += y  x = x + y
+// -=   x -= y  x = x - y
+// *=   x *= y  x = x * y
+// /=   x /= y  x = x / y
+// %=   x %= y  x = x % y
+
+
+var a = 30;
+
+a /= 3;
+
+console.log(a);
