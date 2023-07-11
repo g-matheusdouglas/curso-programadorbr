8122ea7fcbd023af9cb145f6e6036f09e752ab5f
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-07-11 20:33:20 -0300

    Aula12

---
+++ Modulo12 (Javascript - Básico [Lógica de programação])/Aula12 (Alert e Prompt)/script.js
@@ -0,0 +1,11 @@
+// alert('Bom dia, Matheus!')
+/*
+var nome = prompt('Qual o seu nome?')
+alert(`Bom dia, ${nome}!`)
+*/
+
+var numero = parseInt(prompt('Digite um número'))
+
+var dobro = numero + numero
+
+alert(`O dobro de ${numero} é ${dobro}`)
