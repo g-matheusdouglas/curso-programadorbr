a15c2da13140f0f7d48ad4527bab258d371df8f3
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-07-11 22:28:34 -0300

    Aula16

---
+++ Modulo12 (Javascript - Básico [Lógica de programação])/Aula16 (Repetição - FOR e WHILE)/script.js
@@ -0,0 +1,21 @@
+
+var numero = 5;
+
+// FOR
+/* 
+for(var i = 1; i <= numero; i++) {
+    console.log(`${i}ª vez executando o for`)
+}
+ */
+
+//WHILE
+
+var cont = 0
+
+while(cont <= numero) {
+    console.log(`Número ${cont}`)
+    cont++
+}
+
+
+console.log('Acabou')
