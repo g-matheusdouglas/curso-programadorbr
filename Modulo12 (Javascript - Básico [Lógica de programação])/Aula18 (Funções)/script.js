e51b94b12bb31a9e4c1296f420d0044968da5756
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-07-14 22:57:55 -0300

    Aula18

---
+++ Modulo12 (Javascript - Básico [Lógica de programação])/Aula18 (Funções)/script.js
@@ -0,0 +1,16 @@
+
+
+
+function media(n1, n2) {
+    var nota1 = n1;
+    var nota2 = n2;
+    var media = (nota1 + nota2) / 2;
+    /* console.log(media); */
+    return media;
+}
+
+
+var resultado1 = media(6, 7)
+var resultado2 = media(7, 8)
+
+console.log(resultado1 + ' E ' + resultado2)
