65ff2ad235a68a3847d0c99b29f8f15c6f32a7a7
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-07-16 21:56:05 -0300

    Aula23

---
+++ Modulo12 (Javascript - Básico [Lógica de programação])/Aula23 (Objetos - Construtores)/script.js
@@ -0,0 +1,44 @@
+/* 
+function criarAluno(aluno, n1, n2) {
+    return {
+        nome: aluno,
+        nota1: n1,
+        nota2: n2,
+        media: function() {
+            return (this.nota1 + this.nota2) / 2
+        }
+    }
+}
+ */
+
+function aluno(nome, n1, n2) {
+    this.nome = nome
+    this.nota1 = n1
+    this.nota2 = n2
+
+    this.media = function () {
+        return (this.nota1 + this.nota2) / 2
+    }
+}
+
+/* 
+var turma = [
+    criarAluno('Matheus', 9, 7),
+    criarAluno('João', 7, 4),
+    criarAluno('Gizele', 8, 7.5)
+]
+
+var aluno = turma[2]
+ */
+
+var a = new aluno('Igor', 8, 7)
+var a = new aluno('João', 6, 7)
+
+console.log(a)
+
+/* 
+turma.forEach(
+    function (elemento) {
+        console.log(`${elemento.nome} - ${elemento.media()}`)
+    }
+) */
