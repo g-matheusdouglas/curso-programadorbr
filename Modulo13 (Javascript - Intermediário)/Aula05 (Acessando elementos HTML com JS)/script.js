44478331d18242d914b2337f32c0e040b3ab1d22
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-07-19 15:51:47 -0300

    Aula05

---
+++ Modulo13 (Javascript - Intermediário)/Aula05 (Acessando elementos HTML com JS)/script.js
@@ -0,0 +1,11 @@
+let p1 = document.getElementById('p1')
+
+let paragrafos = document.getElementsByClassName('paragrafo')
+
+for(let p of paragrafos) {
+    console.log(p)
+}
+
+let th = document.getElementsByTagName('h1')
+
+let tp = document.getElementsByTagName('p')
