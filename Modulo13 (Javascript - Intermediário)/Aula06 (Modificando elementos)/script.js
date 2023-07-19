b3c4c764593e813785b359d23fbc8991f8e4661e
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2023-07-19 16:22:07 -0300

    Aula06

---
+++ Modulo13 (Javascript - Intermediário)/Aula06 (Modificando elementos)/script.js
@@ -0,0 +1,15 @@
+let p = document.getElementById('p1')
+
+/* p.innerHTML = '<h1>Novo conteúdo</h2>' // Vai receber um HTML
+
+p.innerText = '<h2>Conteúdo mais novo</h2>' // Vai receber um TEXTO como está aparecendo na tela
+
+p.textContent = '<h2>Mais novo ainda</h2>' // Vai receber um TEXTO como está aparecendo no HTML */
+
+p.className = ''
+
+p.style = 'color: blue'
+
+p.setAttribute('meuAtt','Outro nome')
+
+p.setAttribute('novoAtt', 'Qualquer coisa')
