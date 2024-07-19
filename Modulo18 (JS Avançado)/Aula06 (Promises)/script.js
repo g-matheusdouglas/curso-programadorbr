1ac59783e67f12b94d09b32ebc2d4cf40692df40
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2024-07-19 11:36:50 -0300

    Aula06 Promises

---
+++ Modulo18 (JS Avançado)/Aula06 (Promises)/script.js
@@ -0,0 +1,26 @@
+let usuarios = ['Adriano', 'Marcia', 'Jose'];
+
+function inserirUsuario(nome) {
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            usuarios.push(nome); 
+            let erro = true;
+            if(!erro) {
+                resolve();
+            }else {
+                reject({msg: 'Erro de qualquer coisa'});
+            }
+        }, 1000);
+    })
+    return promise;
+}
+
+function listarUsuarios() {
+    console.log(usuarios);
+}
+
+inserirUsuario('Matheus')
+    .then(listarUsuarios)
+    .catch((error) => {
+    console.log(error.msg)
+});
